d74cdce41ec7b625c1e4b4d96c93819e68a8c7f7
Tim Littau <113526482+tmlittau@users.noreply.github.com>
2025-10-10 16:41:43 +0200

    Fix tableau to circuit fallback

--- quasar/conversion/tab2dd.py
+++ quasar/conversion/tab2dd.py
@@ -7,7 +7,10 @@ def tableau_to_dd(tableau_like: Any) -> Optional[Any]:
     try:
         from qiskit import QuantumCircuit
         if hasattr(tableau_like, "to_circuit"):
-            circuit = tableau_like.to_circuit()
+            try:
+                circuit = tableau_like.to_circuit()
+            except Exception:
+                circuit = None
             if isinstance(circuit, QuantumCircuit):
                 return circuit
         from qiskit.quantum_info import Statevector
